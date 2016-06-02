b6b9f0336d1a7a0875d0eff0e1df9ee1dbd724cf
vaan killer <534895367@qq.com>
2016-06-02 20:01:48 +0800

    add function

--- list/array_list.c
+++ list/array_list.c
@@ -4,3 +4,32 @@
 #include "array_list.h"
 
 
+pListNode list_find(pListNode l, ElemType x)
+{
+	
+}
+
+pListNode list_insert_head(pListNode l, ElemType x)
+{
+	
+}
+
+pListNode list_insert_tail(pListNode l, ElemType x)
+{
+	
+}
+
+pListNode list_delete_head(pListNode head)
+{
+	
+}
+
+pListNode list_delete_tail(pListNode head)
+{
+	
+}
+
+pListNode list_delete_elem(pListNode l, ElemType elem)
+{
+	
+}
