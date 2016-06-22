da477ade293067156a59cde5c05e5facbc078fb4
vaan killer <534895367@qq.com>
2016-06-22 20:51:07 +0800

    add quicksort.c

---
+++ sort/quicksort.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "sort.h"
+
+int partition(int* nums, int start, int end)
+{
+
+}
+
+int quicksort(int* nums, int size)
+{
+	
+}
